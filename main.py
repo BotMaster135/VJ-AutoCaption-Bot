dfad6d433bcebb5aa9bfdb694c15bdefcc9bae16
BotMaster135 <amirbhaiyt1@gmail.com>
2024-11-06 23:22:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def start_buttons(bot, update):
    pyrogram.types.InlineKeyboardButton("Updates", url="t.me/+eEMdl7-M_5RmMWNl"),
    pyrogram.types.InlineKeyboardButton("About 🤠", callback_data="about")
    ],[
-   pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{@autocaptuon50_bot}?startchannel=true")
+   pyrogram.types.InlineKeyboardButton("➕️ Add To Your Channel ➕️", url=f"http://t.me/{autocaptuon50_bot}?startchannel=true")
    ]]
   return pyrogram.types.InlineKeyboardMarkup(buttons)
 
